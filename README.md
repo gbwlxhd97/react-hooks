91f58af1c401f950da8a29a7f95ece116d98839f
gbwlxhd97 <gbwlxhd97@naver.com>
2021-05-03 13:40:54 +0900

    chap 1 use State

--- README.md
+++ README.md
@@ -2,17 +2,18 @@
 
 ## react hooks를 학습하며 NPM 라이브러리에 등록하는 과정까지 학습.
 
-- [ ] useTitle
-- [ ] useInput
-- [ ] usePageLeave
-- [ ] useClick
-- [ ] useFadeIn
-- [ ] useFullscreen
-- [ ] useHover
-- [ ] useNetwork
-- [ ] useNotification
-- [ ] useScroll
-- [ ] useTabs
-- [ ] usePreventLeave
-- [ ] useConfirm
-- [ ] useAxios
+- [O] useState - Hooks를 호출하여 함수 컴포넌트에 state 변수를 선언할 수 있는 함수
+- [ ] useTitle - react document의 title을 몇개의 hooks와 함께 바꾸는것
+- [ ] useInput - just Input 역할
+- [ ] usePageLeave - 유저가 page를 벗어나는 시점을 발견하고 함수를 실행하기
+- [ ] useClick - 누군가 element를 클릭하는 시점을 발견하기
+- [ ] useFadeIn - 어떤 Element든 상관없이 에니메이션을 Element 안으로 서서히 사라지게 만들기
+- [ ] useFullscreen - 어떤 Element든 풀스크린으로 만들거나 일반 화면으로 돌아가게 할 수 있음
+- [ ] useHover - 어떤것에 마우스를 올렸을 때 감지하기
+- [ ] useNetwork - Online or Offline 상태 감지하기
+- [ ] useNotification - notification API를 사용할때 유저에게 알림을 보내주기
+- [ ] useScroll - 스크롤을 사용할 때를 감지해 알려주기
+- [ ] useTabs - 웹사이트에 메뉴 또는 무엇이든 간에 tab을 사용하기 매우 쉽게 만들어주는 것
+- [ ] usePreventLeave - 유저가 변경사항이나 무엇이든간에 저장하지 않고 페이지를 벗어나길 원할때 확인을 하는것
+- [ ] useConfirm - 위와 비슷하지만, 어떤 기능이 존재함
+- [ ] useAxios - HTTP request client axios 를 위한 wrapper
--- src/App.js
+++ src/App.js
@@ -1,5 +1,47 @@
-function App() {
-  return <div className="App"></div>;
+import React, { useState } from "react";
+
+const App = () => {
+  const [item, setItem] = useState(1);
+  const IncrementItem = () => setItem(item + 1);
+  const decrementItem = () => setItem(item - 1);
+  return (
+    <div className="App">
+      <h1>{item}</h1>
+      <button onClick={IncrementItem}>Increse</button>
+      <button onClick={decrementItem}>Decrese</button>
+    </div>
+  );
+};
+// class 형 컴포넌트로 똑같은 기능을 구현할떄 state 관리.
+// eslint-disable-next-line
+class AppUgly extends React.Component {
+  state = {
+    item: 1,
+  };
+  render() {
+    const { item } = this.state;
+    return (
+      <div className="App">
+        <h1>{item}</h1>
+        <button onClick={this.IncrementItem}>Increse</button>
+        <button onClick={this.decrementItem}>Decrese</button>
+      </div>
+    );
+  }
+  IncrementItem = () => {
+    this.setState((state) => {
+      return {
+        item: state.item + 1,
+      };
+    });
+  };
+  decrementItem = () => {
+    this.setState((state) => {
+      return {
+        item: state.item - 1,
+      };
+    });
+  };
 }
 
 export default App;
