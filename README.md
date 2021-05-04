82d1ad697488c45788b320e87733b3c0faaf726a
gbwlxhd97 <gbwlxhd97@naver.com>
2021-05-04 18:10:45 +0900

    chap 5 useClick

--- README.md
+++ README.md
@@ -6,8 +6,8 @@
 - [O] useInput - just Input 역할
 - [O] useTabs - 웹사이트에 메뉴 또는 무엇이든 간에 tab을 사용하기 매우 쉽게 만들어주는 것
 - [O] useTitle - react document의 title을 몇개의 hooks와 함께 바꾸는것
+- [O] useClick - 누군가 element를 클릭하는 시점을 발견하기
 - [ ] usePageLeave - 유저가 page를 벗어나는 시점을 발견하고 함수를 실행하기
-- [ ] useClick - 누군가 element를 클릭하는 시점을 발견하기
 - [ ] useFadeIn - 어떤 Element든 상관없이 에니메이션을 Element 안으로 서서히 사라지게 만들기
 - [ ] useFullscreen - 어떤 Element든 풀스크린으로 만들거나 일반 화면으로 돌아가게 할 수 있음
 - [ ] useHover - 어떤것에 마우스를 올렸을 때 감지하기
--- src/App.js
+++ src/App.js
@@ -1,21 +1,27 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useRef } from "react";
 
-const useTitle = (initialTitle) => {
-  const [title, setTitle] = useState(initialTitle);
-  const updateTitle = () => {
-    const htmlTitle = document.querySelector("title");
-    htmlTitle.innerText = title; //4번라인 title로 받은값으로 변환
-  };
-  useEffect(updateTitle, [title]);
-  return setTitle;
+const useClick = (onClick) => {
+  const element = useRef();
+  useEffect(() => {
+    if (element.current) {
+      //reference 안에 element.current가 있는지 확인한다.
+      element.current.addEventListener("click", onClick);
+    }
+    return () => {
+      if (element.current) {
+        element.current.removeEventListener("click", onClick);
+      }
+    };
+  }, []);
+  return element;
 };
-//initialTitle은 Loading...으로 값 변경
+
 const App = () => {
-  const titleUpdator = useTitle("Loading ..."); //title Updator는 setTitle과 동일함
-  setTimeout(() => titleUpdator("home"), 5000);
+  const sayHello = () => console.log("say hello");
+  const title = useClick(sayHello);
   return (
     <div className="App">
-      <h1>하이</h1>
+      <h1 ref={title}>하이</h1>
     </div>
   );
 };
