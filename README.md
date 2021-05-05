b8b01865688e899c94f48c4e4b7545b9fcf271ae
gbwlxhd97 <gbwlxhd97@naver.com>
2021-05-05 11:54:09 +0900

    chap 6 useConfirm

--- README.md
+++ README.md
@@ -7,6 +7,8 @@
 - [O] useTabs - 웹사이트에 메뉴 또는 무엇이든 간에 tab을 사용하기 매우 쉽게 만들어주는 것
 - [O] useTitle - react document의 title을 몇개의 hooks와 함께 바꾸는것
 - [O] useClick - 누군가 element를 클릭하는 시점을 발견하기
+- [O] useConfirm - 사용자가 무언가를 하기전에 확인 하는것(사용자가 버튼을 클릭하는 작업을 하면 이벤트를 실행하기전에 메세지를 보여줌. -> 정말 실행할거니? 이런식으로..) 다만 hooks가아님 단순한 함수형 프로그래밍임.
+- [ ] usePreventLeave - 유저가 변경사항이나 무엇이든간에 저장하지 않고 페이지를 벗어나길 원할때 확인을 하는것
 - [ ] usePageLeave - 유저가 page를 벗어나는 시점을 발견하고 함수를 실행하기
 - [ ] useFadeIn - 어떤 Element든 상관없이 에니메이션을 Element 안으로 서서히 사라지게 만들기
 - [ ] useFullscreen - 어떤 Element든 풀스크린으로 만들거나 일반 화면으로 돌아가게 할 수 있음
@@ -14,6 +16,4 @@
 - [ ] useNetwork - Online or Offline 상태 감지하기
 - [ ] useNotification - notification API를 사용할때 유저에게 알림을 보내주기
 - [ ] useScroll - 스크롤을 사용할 때를 감지해 알려주기
-- [ ] usePreventLeave - 유저가 변경사항이나 무엇이든간에 저장하지 않고 페이지를 벗어나길 원할때 확인을 하는것
-- [ ] useConfirm - 위와 비슷하지만, 어떤 기능이 존재함
 - [ ] useAxios - HTTP request client axios 를 위한 wrapper
--- src/App.js
+++ src/App.js
@@ -1,27 +1,32 @@
 import React, { useState, useEffect, useRef } from "react";
 
-const useClick = (onClick) => {
-  const element = useRef();
-  useEffect(() => {
-    if (element.current) {
-      //reference 안에 element.current가 있는지 확인한다.
-      element.current.addEventListener("click", onClick);
+const useConfirm = (message = "", onConfirm, onCancel) => {
+  if (!onConfirm || typeof onConfirm !== "function") {
+    //존재하지않거나 function이 아닐때,
+    return; //조건이 일치하면 함수 종료.
+  }
+  if (onCancel || typeof onCancel !== "function") {
+    return;
+  }
+  const confirmAction = () => {
+    if (window.confirm(message)) {
+      //브라우저에 메세지를 가지고 있으면 콜백함수 실행
+      onConfirm();
+    } else {
+      onCancel(); // 거절함수
     }
-    return () => {
-      if (element.current) {
-        element.current.removeEventListener("click", onClick);
-      }
-    };
-  }, []);
-  return element;
+  };
+  return confirmAction;
 };
 
 const App = () => {
-  const sayHello = () => console.log("say hello");
-  const title = useClick(sayHello);
+  const deleteWorld = () => console.log("삭제되어가고있음.");
+  const abort = () => console.log("Abort");
+  const confirmDelete = useConfirm("삭제할거니?", deleteWorld, abort); //3번째 라인 정의한 콜백함수로 deleteworld(onConfirm),abort(onCancel)콜백.
   return (
     <div className="App">
-      <h1 ref={title}>하이</h1>
+      <h1>하이</h1>
+      <button onClick={confirmDelete}>Delete</button>
     </div>
   );
 };
