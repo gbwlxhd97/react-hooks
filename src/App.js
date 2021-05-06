6235ad6f14348e96d48a507743dbbf29461d7a9e
gbwlxhd97 <gbwlxhd97@naver.com>
2021-05-06 11:53:04 +0900

    chap 8 useBeforeLeave

--- src/App.js
+++ src/App.js
@@ -1,23 +1,27 @@
 import React, { useState, useEffect, useRef } from "react";
 
-const usePreventLeave = () => {
-  const listener = (event) => {
-    event.preventDefault();
-    event.returnValue = ""; //크롬에만 있는 기능 스펠링틀리는거 조심.
+const useBeforeLeave = (onBefore) => {
+  if (typeof onBefore !== "function") {
+    return; //onBefore이 함수가 아니면 다 죽임.(함수종료)
+  }
+  const handle = (event) => {
+    const { clientY } = event;
+    if (clientY <= 0) {
+      onBefore();
+    }
   };
-  const enablePrevent = () => window.addEventListener("beforeunload", listener);
-  const disablePrevent = () =>
-    window.removeEventListener("beforeunload", listener);
-  return { enablePrevent, disablePrevent };
+  useEffect(() => {
+    document.addEventListener("mouseleave", handle); //컴포넌트가 mount 되었을 때,
+    return () => document.removeEventListener("mouseleave", handle); //Unmount 되었을 때
+  }, []); //단 한번만 실행하기 위해서 deps를 빈 배열로 선언
 };
 
 const App = () => {
-  const { enablePrevent, disablePrevent } = usePreventLeave();
+  const begForLife = () => console.log("pls dont leave");
+  useBeforeLeave(begForLife);
   return (
     <div className="App">
       <h1>하이</h1>
-      <button onClick={enablePrevent}>보호</button>
-      <button onClick={disablePrevent}>안보호</button>
     </div>
   );
 };
