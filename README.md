81dd595644677c67e53e045158fbabddbb52ff75
gbwlxhd97 <gbwlxhd97@naver.com>
2021-05-06 14:24:02 +0900

    chap 9 useFadeIn

--- README.md
+++ README.md
@@ -10,10 +10,10 @@
 - [O] useConfirm - 사용자가 무언가를 하기전에 확인 하는것(사용자가 버튼을 클릭하는 작업을 하면 이벤트를 실행하기전에 메세지를 보여줌. -> 정말 실행할거니? 이런식으로..) 다만 hooks가아님 단순한 함수형 프로그래밍임.
 - [O] usePreventLeave - 유저가 변경사항이나 무엇이든간에 저장하지 않고 페이지를 벗어나길 원할때 확인을 하는것 다만 이것도 hooks말고 단순 함수형 프로그래밍임.
 - [O] useBeforeLeave - 유저가 page를 벗어나는 시점을 발견하고 함수를 실행하기
-- [ ] useFadeIn - 어떤 Element든 상관없이 에니메이션을 Element 안으로 서서히 사라지게 만들기
+- [O] useFadeIn - 어떤 Element든 상관없이 에니메이션을 Element 안으로 서서히 사라지게 만들기
+- [ ] useNetwork - Online or Offline 상태 감지하기
 - [ ] useFullscreen - 어떤 Element든 풀스크린으로 만들거나 일반 화면으로 돌아가게 할 수 있음
 - [ ] useHover - 어떤것에 마우스를 올렸을 때 감지하기
-- [ ] useNetwork - Online or Offline 상태 감지하기
 - [ ] useNotification - notification API를 사용할때 유저에게 알림을 보내주기
 - [ ] useScroll - 스크롤을 사용할 때를 감지해 알려주기
 - [ ] useAxios - HTTP request client axios 를 위한 wrapper
--- src/App.js
+++ src/App.js
@@ -1,27 +1,30 @@
 import React, { useState, useEffect, useRef } from "react";
 
-const useBeforeLeave = (onBefore) => {
-  if (typeof onBefore !== "function") {
-    return; //onBefore이 함수가 아니면 다 죽임.(함수종료)
-  }
-  const handle = (event) => {
-    const { clientY } = event;
-    if (clientY <= 0) {
-      onBefore();
-    }
-  };
+const useFadeIn = (duration = 1, delay = 0) => {
+  const element = useRef();
+
+  //해당 element 안에 나타나기 위해서 useEffect 사용
   useEffect(() => {
-    document.addEventListener("mouseleave", handle); //컴포넌트가 mount 되었을 때,
-    return () => document.removeEventListener("mouseleave", handle); //Unmount 되었을 때
-  }, []); //단 한번만 실행하기 위해서 deps를 빈 배열로 선언
+    if (typeof duration !== "number" || typeof delay !== "number") {
+      return;
+    }
+    if (element.current) {
+      const { current } = element;
+      current.style.opacity = 1;
+      current.style.transition = `opacity ${duration}s ease-in-out ${delay}s`;
+    }
+  }, [duration, delay]);
+
+  return { ref: element, style: { opacity: 0 } };
 };
 
 const App = () => {
-  const begForLife = () => console.log("pls dont leave");
-  useBeforeLeave(begForLife);
+  const fadeInH1 = useFadeIn(1, 2);
+  const fadeInP = useFadeIn(4, 4);
   return (
     <div className="App">
-      <h1>하이</h1>
+      <h1 {...fadeInH1}>하이</h1>
+      <p {...fadeInP}>lorem itsum 벽력일섬</p>
     </div>
   );
 };
