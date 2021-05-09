5b225f78149c85e8f415b3c38aa1986fd9b74aec
gbwlxhd97 <gbwlxhd97@naver.com>
2021-05-09 15:11:17 +0900

    chap 11 OnScroll

--- src/App.js
+++ src/App.js
@@ -1,33 +1,27 @@
 import React, { useState, useEffect, useRef } from "react";
 
-const useNetwork = (onChange) => {
-  const [status, setStatus] = useState(navigator.onLine);
-  const handleChange = () => {
-    if (typeof onChange === "function") {
-      onChange(navigator.onLine);
-    }
-    setStatus(navigator.onLine);
+const useScroll = () => {
+  const [state, setState] = useState({
+    x: 0,
+    y: 0,
+  });
+  const onScroll = () => {
+    //handle효과
+    setState({ y: window.scrollY, x: window.scrollX }); //setState로 state값 변경을 적용해줌.
   };
   useEffect(() => {
-    window.addEventListener("online", handleChange);
-    window.addEventListener("offline", handleChange);
-    return () => {
-      //Unmount 일때
-      window.removeEventListener("online", handleChange);
-      window.removeEventListener("offline", handleChange);
-    };
+    window.addEventListener("scroll", onScroll);
+    return () => window.removeEventListener("scroll", onScroll); //event를 추가했으면 반드시 같은 event,함수명 동일하게 지워줘야함.
   }, []);
-  return status;
+
+  return state;
 };
 
 const App = () => {
-  const handleNetworkChange = (online) => {
-    console.log(online ? "online!" : "offline!");
-  };
-  const onLine = useNetwork(handleNetworkChange);
+  const { y } = useScroll();
   return (
-    <div className="App">
-      <h1>{onLine ? "Online" : "Offline"}</h1>
+    <div className="App" style={{ height: "1000vh" }}>
+      <h1 style={{ position: "fixed", color: y > 100 ? "red" : "blue" }}>hi</h1>
     </div>
   );
 };
